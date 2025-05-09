94fcfc9ad3a49604bd2b85ed5460b53d2ed8c7de
EkeneDeProgram <136784975+EkeneDeProgram@users.noreply.github.com>
2025-05-09 21:23:31 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,12 @@
-output "kubeconfig" {
-  value = aws_eks_cluster.eks_cluster.kubeconfig[0].value
+output "ec2_public_ip" {
+  value = aws_instance.django_server.public_ip
 }
+
+data "aws_db_instance" "existing_postgres" {
+  db_instance_identifier = "django-db"  # Your DB instance identifier
+}
+
+output "rds_endpoint" {
+  value = data.aws_db_instance.existing_postgres.endpoint  # This will fetch the endpoint of your RDS
+}
+
